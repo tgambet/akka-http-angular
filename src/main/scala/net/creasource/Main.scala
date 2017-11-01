5059cce1cdf0606ce5c244ec5cb56523a4028eaa
Thomas Gambet <thomas.gambet@gmail.com>
2017-11-01 15:31:31 +0100

    reintroduced stopOnReturn...

--- src/main/scala/net/creasource/Main.scala
+++ src/main/scala/net/creasource/Main.scala
@@ -3,10 +3,10 @@ package net.creasource
 import akka.http.scaladsl.Http
 
 import scala.concurrent.Future
+import scala.io.StdIn
+
 import net.creasource.api.Application
 import net.creasource.http.WebServer
-
-import scala.io.StdIn
 
 object Main extends App with WebServer {
 
@@ -14,6 +14,7 @@ object Main extends App with WebServer {
 
   private val host = app.conf.getString("http.host")
   private val port = app.conf.getInt("http.port")
+  private val stopOnReturn  = app.conf.getBoolean("http.stopOnReturn")
 
   val bindingFuture: Future[Http.ServerBinding] = Http().bindAndHandle(routes, host, port)
 
@@ -25,15 +26,15 @@ object Main extends App with WebServer {
     app.system.log.error(ex, "Failed to bind to {}:{}!", host, port)
   }
 
-  println(s"Press RETURN to stop...")
-  StdIn.readLine()
-
-  bindingFuture
-    .flatMap(_.unbind())
-    .onComplete { _ =>
-      killSwitch.shutdown()
-      app.shutdown()
-    }
-
+  if (stopOnReturn) {
+    println(s"Press RETURN to stop...")
+    StdIn.readLine()
+    bindingFuture
+      .flatMap(_.unbind())
+      .onComplete { _ =>
+        killSwitch.shutdown()
+        app.shutdown()
+      }
+  }
 
 }
