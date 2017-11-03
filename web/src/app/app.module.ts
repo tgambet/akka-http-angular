c1f2192bdeb71fb937c58561180fca40cab1e26c
Thomas Gambet <thomas.gambet@gmail.com>
2017-11-03 13:11:07 +0100

    import some material components

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }                from '@angular/core';
 import { BrowserModule }           from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import * as Material               from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    Material.MatMenu,
+    Material.MatSidenav,
+    Material.MatToolbar
   ],
   providers: [],
   bootstrap: [AppComponent]
