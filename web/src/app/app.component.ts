6bb9538c75e145ac90f0b9658f93156955033864
Thomas Gambet <thomas.gambet@gmail.com>
2017-11-03 20:08:26 +0100

    added a breakpoint observer to open/close sidenav depending on screen

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { webSocket }        from 'rxjs/observable/dom/webSocket'
-import { WebSocketSubject } from 'rxjs/observable/dom/WebSocketSubject'
+import { Component, OnInit, ViewChild }    from '@angular/core';
+import { webSocket }                       from 'rxjs/observable/dom/webSocket'
+import { WebSocketSubject }                from 'rxjs/observable/dom/WebSocketSubject'
+import * as Material                       from '@angular/material';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -10,24 +12,44 @@ import { WebSocketSubject } from 'rxjs/observable/dom/WebSocketSubject'
 export class AppComponent implements OnInit {
   title = 'app';
 
+  @ViewChild("sidenav")
+  sidenav: Material.MatSidenav;
+
+  sidenavMode: String = "over";
+
   subject: WebSocketSubject<string>;
+
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe([
+      Breakpoints.Web
+    ]).subscribe(result => {
+      if (!result.matches) {
+        this.sidenavMode = "over";
+        this.sidenav && this.sidenav.close();
+      } else {
+        this.sidenavMode = "side";
+        this.sidenav && this.sidenav.open();
+      }
+    });
+  }
 
   ngOnInit(): void {
 
-    this.subject = webSocket('ws://localhost:8080/socket');
 
-    let subscription =
-      this.subject
-        .subscribe(
-          (msg) => console.log(msg),
-          (err) => console.log(err),
-          () => console.log('complete')
-        );
-
-    setTimeout(() => this.subject.next('test'), 1000)
-    setTimeout(() => this.subject.next('test'), 1000)
-    setTimeout(() => this.subject.next('test'), 1000)
-    setTimeout(() => this.subject.next('test'), 1000)
+    // this.subject = webSocket('ws://localhost:8080/socket');
+    //
+    // let subscription =
+    //   this.subject
+    //     .subscribe(
+    //       (msg) => console.log(msg),
+    //       (err) => console.log(err),
+    //       () => console.log('complete')
+    //     );
+    //
+    // setTimeout(() => this.subject.next('test'), 1000)
+    // setTimeout(() => this.subject.next('test'), 1000)
+    // setTimeout(() => this.subject.next('test'), 1000)
+    // setTimeout(() => this.subject.next('test'), 1000)
 
   }
 
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }                from '@angular/core';
 import { BrowserModule }           from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import * as Material               from '@angular/material';
+import { BreakpointObserver, MediaMatcher } from '@angular/cdk/layout';
 
 import { AppComponent } from './app.component';
 
@@ -17,7 +18,10 @@ import { AppComponent } from './app.component';
     Material.MatToolbarModule,
     Material.MatIconModule
   ],
-  providers: [],
+  providers: [
+    MediaMatcher,
+    BreakpointObserver
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
